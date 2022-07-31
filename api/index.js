3e1abe9a77c89fda3af98141f4da54d0bc666f73
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-07-31 12:13:52 +0200

    Debug 🔧

--- api/index.js
+++ api/index.js
@@ -7,9 +7,9 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(router);
 app.use(morgan("dev"));
-app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => console.log(`🚀 Listening on port ${port}...`));
